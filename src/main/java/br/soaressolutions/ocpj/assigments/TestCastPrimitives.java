6589bb8fa113a949d145fc6e45a11886fe28ddc7
soaresma <silveirama.soares@gmail.com>
2016-01-26 16:45:34 -0200

    new code

---
+++ src/main/java/br/soaressolutions/ocpj/assigments/TestCastPrimitives.java
@@ -0,0 +1,15 @@
+package br.soaressolutions.ocpj.assigments;
+
+
+public class TestCastPrimitives {
+	
+	public static void main(String[] args) {
+		float f = 234.56F;
+		short s = (short) f;
+		
+		System.out.println("f = " + f);
+		System.out.println("s = " + s);
+	}
+	
+
+}
